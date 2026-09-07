40685c5efff8894f90b404505a997fcb8d3bb525
smhmi <118555758+smhmi@users.noreply.github.com>
2026-09-07 15:24:43 +0330

    Update data.js

--- data.js
+++ data.js
@@ -248,7 +248,7 @@ const EMPLOYEES = {
 
   "177": {
     employeeName: "حسن نیگسان",
-    password: "177",
+    password: "0064830276",
     payslips: {
       "فروردین": {work: "1405-01-work.jpg", slip: "1405-01-slip.jpg", pay: "1405-01-pay.jpg"},
       "اردیبهشت": {work: "1405-02-work.jpg", slip: "1405-02-slip.jpg", pay: "1405-02-pay.jpg"},
